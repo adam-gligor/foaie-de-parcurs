fc191eb5d6b6aa3dd89224fb2ef707c5f6b68388
adam gligor <gligor.adam@gmail.com>
2018-07-18 15:07:09 +0300

    temp disable service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import * as RoutesModule from './routes';
 
 const routes = RoutesModule.routes;
@@ -13,4 +13,4 @@ ReactDOM.render(
   document.getElementById('root') as HTMLElement
 );
 
-registerServiceWorker();
+// registerServiceWorker();
